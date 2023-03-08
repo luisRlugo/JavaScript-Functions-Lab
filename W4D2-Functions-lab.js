7542aced3fc9465a634381527c6aeca9ad8920ec
Luis <roberto_lugo95@yahoo.com>
2023-03-07 23:28:44 -0600

    Functions Lab answered

--- W4D2-Functions-lab.js
+++ W4D2-Functions-lab.js
@@ -4,12 +4,35 @@
 // minusOne(100);       // 99
 // minusOne(Infinity);  // Infinity
 
-// Write a function makeSentencethat takes three parameters and concatenates them into a fully formed sentence.
+function minusOne(num) {
+  console.log(num - 1);
+}
+minusOne(10);
+minusOne(Infinity);
+minusOne(100);
+
+console.log("==================================");
+
+// Write a function makeSentence that takes three parameters and concatenates them into a fully formed sentence.
 
 // makeSentence('I', 'want', 'chimichangas');
 // => 'Oh boy, do I want chimichangas or what?'
+//function expression
+const makeSentence = function (word1, word2, word3) {
+  console.log("oh boy, do " + word1 + " " + word2 + " " + word3 + " or what?");
+};
 
-// Write a function called getLastElementthat takes a parameter arr.
+makeSentence("I", "want", "chimichangas");
+
+//function declaration
+// function makeSentence(word1, word2, word3) {
+//   console.log("oh boy, do " + word1 + " " + word2 + " " + word3 + " or what?");
+// }
+// makeSentence("I", "want", "chimichangas");
+
+console.log("==================================");
+
+// Write a function called getLastElement that takes a parameter arr.
 // Invoke the function with an array as the argument.
 // The function should print the last element within the array.
 
@@ -17,3 +40,14 @@
 // getLastElement(['a', 'b', 'c']);          // 'c'
 // getLastElement([[1, 2, 3], [4, 5, 6]]);   // [4, 5, 6]
 // Hint: arr[arr.length - 1]-->
+
+function getLastElement(arr) {
+  console.log(arr[arr.length - 1]);
+}
+
+getLastElement([1, 2, 3, 4, 5, 6]);
+getLastElement(["a", "b", "c"]);
+getLastElement([
+  [1, 2, 3],
+  [4, 5, 6],
+]);
